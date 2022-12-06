edf2de2f491e1ca918b01214e8fe064001853d17
sterrettJD <sterrettjd@protonmail.com>
2022-12-06 08:26:15 -0700

    add comments

--- Day-6.jl
+++ Day-6.jl
@@ -4,9 +4,12 @@ f = open(file)
 
 
 function find_packet_start(line)
+    # create a sliding window along the line
     for i in 1:length(line)-3
         start = i
         stop = i+3
+        # if there are 4 unique characters in our window, 
+        # break and return the index
         if length(unique(line[start:stop]))==4
             println("SIGNAL after ", stop)
             return stop
