a8577a2bf40b762ae39342399e4a94616a4214a3
sterrettJD <sterrettjd@protonmail.com>
2022-12-08 12:26:34 -0700

    complete part 2

--- Day-8.jl
+++ Day-8.jl
@@ -33,6 +33,61 @@ function get_total_vis(mat)
     return visible_counter
 end
 
+
+function get_scenic_score(tree, row, col, row_index, col_index)
+    view_left = view_right = view_up = view_down = 0
+    # left
+    for i in range(row_index-1, 1, step=-1)
+        view_left += 1
+        if row[i] >= tree
+            break
+        end
+    end
+    # right
+    for i in range(row_index+1, length(row))
+        view_right += 1
+        if row[i] >= tree
+            break
+        end
+    end
+    # up
+    for i in range(col_index-1, 1, step=-1)
+        view_up += 1
+        if col[i] >= tree
+            break
+        end
+    end
+    # down
+    for i in range(col_index+1, length(col))
+        view_down += 1
+        if col[i] >= tree
+            break
+        end
+    end
+
+    views = [view_left, view_right,
+            view_up, view_down]
+    
+    scenic_score = prod(views)
+    return scenic_score    
+end
+
+function get_max_scenic_score(mat)
+    max_score = 0
+    nrow, ncol = size(mat)
+    for i in 1:nrow
+        for j in 1:ncol
+            this_score = get_scenic_score(mat[i,j],
+                                          mat[i,:],
+                                          mat[:,j],
+                                          j,
+                                          i)
+            max_score = max(max_score, this_score)
+        end
+    end
+    return max_score
+end
+
 function solution(file)
     f = open(file)
     
@@ -51,6 +106,8 @@ function solution(file)
     total_visible = get_total_vis(mat)
     println("Number of visible trees: ", total_visible)
 
+    score = get_max_scenic_score(mat)
+    println("Maximum scenic score: ", score)
 end
 
 
